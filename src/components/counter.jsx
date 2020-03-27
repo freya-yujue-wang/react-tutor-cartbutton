b04f190dd021654b6ef88712ef23deed63896b0e
kalidasyan <yujuewa@gmail.com>
2020-03-27 00:00:23 -0700

    add counters componment

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,30 +2,43 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    count: 0,
-    tags: ["tag1", "tag2", "tag3"]
+    value: this.props.value
   };
 
-  renderTags() {
-    if (this.state.tags.length === 0) {
-      return <p>There are no tag!</p>;
-    }
-    return (
-      <ul>
-        {this.state.tags.map(tag => (
-          <li key={tag}>{tag}</li>
-        ))}
-      </ul>
-    );
-  }
+  //2 way to bind : 1. use constructor 2. use arrow function
+  // constructor() {
+  //   super();
+  //   this.handleIncrement = this.handleIncrement.bind(this);
+  // }
+
+  handleIncrement = product => {
+    //console.log("Increment Clicked", this);
+    this.setState({ value: this.state.value + 1 });
+  };
 
   render() {
     return (
       <div>
-        {this.state.tags.length === 0 && "Please create a new tag!"}
-        {this.renderTags()}
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <button
+          onClick={this.handleIncrement}
+          className="btn btn-secondary btn-sm"
+        >
+          Increment
+        </button>
       </div>
     );
+  }
+
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.value === 0 ? "warning" : "primary";
+    return classes;
+  }
+
+  formatCount() {
+    const { value: count } = this.state;
+    return count === 0 ? "Zero" : count;
   }
 }
 
